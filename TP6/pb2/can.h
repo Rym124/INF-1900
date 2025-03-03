812d2ae92086c4e59536b5f35a0b719f01ed19a0
Rym124 <fortasrym12@gmail.com>
2025-03-03 06:02:24 -0500

    Update can.h

--- TP6/pb2/can.h
+++ TP6/pb2/can.h
@@ -1,1 +1,53 @@
+/*
+ * Classe permettant l'acces au convertisseur analogique/numerique
+ * du microcontroleur ATMega16 de Atmel.
+ *
+ * Ecole Polytechnique de Montreal
+ * Departement de genie informatique
+ * Cours inf1995
+ *
+ * Matthew Khouzam et Jerome Collin
+ * 2005-2006
+ *
+ * Code qui n'est sous aucune license.
+ *
+ */
 
+#ifndef E6470A02_C67C_4074_8D0F_E58219DA701D
+#define E6470A02_C67C_4074_8D0F_E58219DA701D
+
+#ifndef CAN_H
+#define CAN_H
+
+#include <avr/io.h> 
+
+/*
+ * Classe can:
+ *   Le constructeur initialise le convertisseur.
+ *   Une lecture enclanche une conversion et le resultat
+ *   est retourne sur 16 bits.
+ *
+ */
+
+class can
+{
+public:
+
+   can();
+   ~can();
+
+   // retourne la valeur numerique correspondant a la valeur
+   // analogique sur le port A.  pos doit etre entre 0 et 7
+   // inclusivement.  Seulement les 10 bits de poids faible
+   // sont significatifs.
+   uint16_t lecture(uint8_t pos);
+
+private:
+   // Donnees membres - aucun
+
+};
+
+#endif /* CAN_H */
+
+
+#endif /* E6470A02_C67C_4074_8D0F_E58219DA701D */
