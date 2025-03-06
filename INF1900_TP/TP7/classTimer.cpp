d673921d8fb22d5310f97fa46a92c0436a82ddbd
Rym124 <fortasrym12@gmail.com>
2025-03-06 13:47:01 -0500

    partie rym fini

--- INF1900_TP/TP7/classTimer.cpp
+++ INF1900_TP/TP7/classTimer.cpp
@@ -3,52 +3,83 @@
 #include <util/delay.h>
 #include "classTimer.h"
 
-void ajustementPwm( uint16_t dureeA , uint16_t dureeB) {
-
-    // mise à un des sorties OC1A et OC1B sur comparaison
-    // mise à un des sorties OC1A et OC1B sur comparaison
-
-    // réussie en mode PWM 8 bits, phase correcte
-
-    // et valeur de TOP fixe à 0xFF (mode #1 de la table 16-5
-
-    // page 130 de la description technique du ATmega324PA)
-
-    OCR1A = dureeA;
-
-    OCR1B = dureeB;
 
 
-    // division d'horloge par 8 - implique une fréquence de PWM fixe
+Timers::Timers(TypeTimer type, ModeTimer mode, uint16_t valeurCtc, uint16_t prescaler)
+    : leType_(type), leMode_(mode), valeurCtc_(valeuCtc) {
+    prescaler_= gChoisirPrescaler(prescaler);
+    if (leType_ == TIMER1) {
+        leMode_ = CTC;  // TIMER1 doit être en mode CTC pour calculer le delais
+    } else if (leType_ == TIMER0 || leType_ == TIMER2) {
+        leMode_ = PWM;  // TIMER0 et TIMER2 doivent être en mode PWM vu qu'on ne peut pas utiliser ;etimer1
+    }
+    if (mode == CTC) {
+        configurationCTC();
+    } else if (mode == PWM) {
+        configurationPWM();
+    } //else if(mode == NORMAL){
+    //     configurationNormal();
+    // }
+} 
 
-    TCCR1A |= (1 << COM1A0) | (1 << COM1B0) | (1 << WGM10); //| (1 << COM1A0) | (1 << COM1B0) ;
+uint16_t Timers:: ChoisirPrescaler(uint16_t valeur){ //page 131 
+    switch (leType_)
+    {
+    // case TIMER1:
+    //     if (valeur == 1){return (1<<CS10)};
+    //     if(valeur == 8){return (1<<CS11)};
+    //     if(valeur == 64){return (1<<CS10)|(1<<CS11)}
+    //     if (valeur == 256){return (1<<CS12)}
+    //     if (valeur ==1024){return (1<<CS10)|(1<<CS12)}
+    //     break;
+    
+    case TIMER0:
+        if (valeur == 1){return (1<<CS00)};
+        if(valeur == 8){return (1<<CS01)};
+        if(valeur == 64){return (1<<CS00)|(1<<CS01)}
+        if (valeur == 256){return (1<<CS02)}
+        if (valeur ==1024){return (1<<CS00)|(1<<CS02)}
+     
+       break;
+    
+    case TIMER2:
+        if (valeur == 1){return (1<<CS20)};
+        if(valeur == 8){return (1<<CS21)};
+        if(valeur == 64){return (1<<CS20)|(1<<CS21)}
+        if (valeur == 256){return (1<<CS22)}
+        if (valeur == 1024){return (1<<CS20)|(1<<CS22)}
+    
+        break;
 
-    TCCR1B |= (1 << CS11) ;
+    }
+    return 0;
+}
 
-    TCCR1C = 0;
+void Timers::configurationPWM(){ //130
+    switch (leType_){
+        case TIMER0:
+        TCCR0A = (1 << COM0A1) | (1 << COM0B1) | (1 << WGM00);
+        TCCR0B = (1 << CS01);    
+        //TCCR0C = 0;
+        break;
+        case TIMER2:
+
+        TCCR2A = (1 << COM2A1) | (1 << COM2B1) | (1 << WGM20);
+        TCCR2B = (1 << CS21);
+        //TCCR0C = 0;
+        break;
+    }
+}
+void Timers::ajustementPwm( uint8_t dureeA , uint8_t dureeB) {
+
+    OCR1A = (dureeA / 100.0) * 255; // gauche
+    OCR1B = (dureeB / 100.0) * 255; // droite
+    // on peut mettre le dutycycle ici 
+
+    configurationPWM();
 }
 
 
-void partirMinuterie(uint16_t duree)  //il faut qu'il soit capable de choisir un prescaler
-{
 
-    gMinuterieExpiree = 0;
 
-    // mode CTC du timer 1 avec horloge divisée par 1024
 
-    // interruption après la durée spécifiée
-
-    TCNT1 = 0;
-
-    OCR1A = duree;
-
-    TCCR1A = 0;
-
-    TCCR1B = (1 << WGM12) | (1 << CS12) | (1 << CS10); // ctc prescaler 1024
-
-    TCCR1C = 0;
-
-    TIMSK1 = (1 << OCIE1A);
-}
-
-// choisir un prescaler :
--- INF1900_TP/TP7/classTimer.h
+++ INF1900_TP/TP7/classTimer.h
@@ -8,26 +8,28 @@ class Timers{
     public:
 
     enum TypeTimer{TIMER1, TIMER0, TIMER2};
-    enum ModeTimer{NORMAL,CTC, PWM};
+    enum ModeTimer{/*NORMAL*/,CTC, PWM};
 
     Timers(TypeTimer type, ModeTimer mode, uint16_t valeurCtc=0, uint16_t prescaler =0){};
 
     void Partircompteur();
     void ArretCopmetur();
 
-    void ChoisirPrescaler(int valeur); // valeur c'est 1024 ou 64 ou8 ou... et la méthode te donne ;es registres à utiliser 
+    void ChoisirPrescaler(uint16_t valeur); // valeur c'est 1024 ou 64 ou8 ou... et la méthode te donne ;es registres à utiliser 
     
     void DutyCylcle(int pourcentage ); // pour le PWM, ici on return les registres dependamment du timer
+    void ajustementPwm( uint8_t dureeA , uint8_t dureeB);
 
     private:
-    uint16_t valeurCtc_;
+    uint16_t valeurCtc_; //valeur de comparaison 
     ///uint16_t valeurNormal_; on fini et on rajoute le normal ok!
-    TypeTimer leType;
-    ModeTimer leMode;
+    TypeTimer leType_;
+    ModeTimer leMode_;
+    uint16_t prescaler_;
     
     void ConfigurationPWM(); //pas accesible 
     void ConfigurationCTC();
-    //void ConfigurationNormal(); pas sure de l'utiliser encode 
+    //void ConfigurationNormal(); //pas sure de l'utiliser encore 
 
 }
 #endif
