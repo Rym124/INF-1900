43e01c2d85cd5057bb706ce6dbe5ddc0448bd8cc
Marily Lemire <marily.lemire@icloud.com>
2025-03-09 17:46:28 -0400

    timer

--- INF1900_TP/TP7/classTimer.cpp
+++ INF1900_TP/TP7/classTimer.cpp
@@ -3,19 +3,27 @@
 #include <util/delay.h>
 #include "classTimer.h"
 
+//Rym
+Timers::Timers(TypeTimer type, ModeTimer mode, uint16_t valeurCtc, uint16_t prescaler)
+    : leType_(type), leMode_(mode), valeurCtc_(valeuCtc) {
+    prescaler_= gChoisirPrescaler(prescaler);
+    if (leType_ == TIMER1) {
+        leMode_ = CTC;  // TIMER1 doit être en mode CTC pour calculer le delais
+    } else if (leType_ == TIMER0 || leType_ == TIMER2) {
+        leMode_ = PWM;  // TIMER0 et TIMER2 doivent être en mode PWM vu qu'on ne peut pas utiliser ;etimer1
+    }
+    if (mode == CTC) {
+        configurationCTC();
+    } else if (mode == PWM) {
+        configurationPWM();
+    } //else if(mode == NORMAL){
+    //     configurationNormal();
+    // }
+} 
+
+//Marily
 void Timers::PartirCompteur(){
 
-    //switch (leType) {
-    //case TIMER0:
-    //    TCCR0B |= (1 << CS02) | (1 << CS00); // p.104 prescaler 1/1024 (ou /8)
-    //    break;
-    //case TIMER1:
-    //    TCCR1B |= (1 << CS12) | (1 << CS10);
-    //    break;
-    //case TIMER2:
-    //    TCCR2B |= (1 << CS22) | (1 << CS20);
-    //    break;
-    //}
     switch (leType) {
     case TIMER0:
         TCCR0B |= prescaler_; //partir compteur en activant le prescaler (ChoisirPrescaler)
@@ -35,17 +43,54 @@ void Timers::ArretCompteur() {
     switch (leType) {
     case TIMER0:
         TCCR0B &= ~((1 << CS02) | (1 << CS01) | (1 << CS00)); //tous bits a 0 = desactive prescaler
+        //TIMSK0 = 0;
         break;
     case TIMER1:
         TCCR1B &= ~((1 << CS12) | (1 << CS11) | (1 << CS10));
+        //TIMSK1 =0;
         break;
     case TIMER2:
         TCCR2B &= ~((1 << CS22) | (1 << CS21) | (1 << CS20));
+        //TIMSK2 = 0;
         break;
     }
 }
 
+//Rym
+uint16_t Timers:: ChoisirPrescaler(uint16_t valeur){ //page 131 
+    switch (leType_)
+    {
+    // case TIMER1:
+    //     if (valeur == 1){return (1<<CS10)};
+    //     if(valeur == 8){return (1<<CS11)};
+    //     if(valeur == 64){return (1<<CS10)|(1<<CS11)}
+    //     if (valeur == 256){return (1<<CS12)}
+    //     if (valeur ==1024){return (1<<CS10)|(1<<CS12)}
+    //     break;
+    
+    case TIMER0:
+        if (valeur == 1){return (1<<CS00)};
+        if(valeur == 8){return (1<<CS01)};
+        if(valeur == 64){return (1<<CS00)|(1<<CS01)}
+        if (valeur == 256){return (1<<CS02)}
+        if (valeur ==1024){return (1<<CS00)|(1<<CS02)}
+     
+       break;
+    
+    case TIMER2:
+        if (valeur == 1){return (1<<CS20)};
+        if(valeur == 8){return (1<<CS21)};
+        if(valeur == 64){return (1<<CS20)|(1<<CS21)}
+        if (valeur == 256){return (1<<CS22)}
+        if (valeur == 1024){return (1<<CS20)|(1<<CS22)}
+    
+        break;
 
+    }
+    return 0;
+}
+
+//Marily
 void Timers::DutyCylcle(int pourcentage){
 
     //jsp si c necessaire:
@@ -68,7 +113,33 @@ void Timers::DutyCylcle(int pourcentage){
 
 }
 
+//Rym
+void Timers::ajustementPwm( uint8_t dureeA , uint8_t dureeB) {
 
+    OCR1A = (dureeA / 100.0) * 255; // gauche
+    OCR1B = (dureeB / 100.0) * 255; // droite
+    // on peut mettre le dutycycle ici 
+
+    configurationPWM();
+}
+
+void Timers::configurationPWM(){ //130
+    switch (leType_){
+        case TIMER0:
+        TCCR0A = (1 << COM0A1) | (1 << COM0B1) | (1 << WGM00);
+        TCCR0B = (1 << CS01);    
+        //TCCR0C = 0;
+        break;
+        case TIMER2:
+
+        TCCR2A = (1 << COM2A1) | (1 << COM2B1) | (1 << WGM20);
+        TCCR2B = (1 << CS21);
+        //TCCR0C = 0;
+        break;
+    }
+}
+
+//Marily
 void Timers::ConfigurationCTC() { //mode ctc -> bit WGMx1 = 1 et OCRxA = valeurCtc
 
     switch (leType) {
