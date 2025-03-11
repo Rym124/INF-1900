30d35bdef8ebf2daf44bbf57ef8a05a6157f5334
Marily Lemire <marily.lemire@icloud.com>
2025-03-11 11:26:42 -0400

    timer h

--- INF1900_TP/TP7/classTimer.h
+++ INF1900_TP/TP7/classTimer.h
@@ -3,31 +3,36 @@
 #ifndef CLASSTIMER_H
 #define CLASSTIMER_H
 
+enum TypeTimer{TIMER0, TIMER1, TIMER2};
+enum ModeTimer{NORMAL,CTC, PWM};
 class Timers{
 
     public:
 
-    enum TypeTimer{TIMER1, TIMER0, TIMER2};
-    enum ModeTimer{NORMAL,CTC, PWM};
 
-    Timers(TypeTimer type, ModeTimer mode, uint16_t valeurCtc=0, uint16_t prescaler =0){};
 
-    void PartirCompteur();
-    void ArretCompteur();
+    Timers(TypeTimer type, ModeTimer mode, uint16_t valeurCtc=0, uint16_t prescaler =0);
 
-    void ChoisirPrescaler(int valeur); // valeur c'est 1024 ou 64 ou8 ou... et la méthode te donne ;es registres à utiliser 
-    
-    void DutyCylcle(int pourcentage ); // pour le PWM, ici on return les registres dependamment du timer
+    void partirCompteur();
+    void arretCompteur();
+
+    uint16_t choisirPrescaler(uint16_t ); // valeur c'est 1024 ou 64 ou8 ou... et la méthode te donne ;es registres à utiliser 
+
+    void dutyCycle(uint16_t ,uint16_t ); // pour le PWM, ici on return les registres dependamment du timer
+    void ajustementPwm( uint8_t  , uint8_t );
+    uint16_t calculPourcentage(uint16_t  , uint16_t );
 
     private:
-    uint16_t valeurCtc_;
-    ///uint16_t valeurNormal_; on fini et on rajoute le normal ok!
+    ///uint16t valeurNormal; on fini et on rajoute le normal ok!
     TypeTimer leType;
     ModeTimer leMode;
-    
-    void ConfigurationPWM(); //pas accesible 
-    void ConfigurationCTC();
-    //void ConfigurationNormal(); pas sure de l'utiliser encode 
+    uint16t valeurCtc; //valeur de compara
+    uint16t prescaler;
 
-}
+
+    void configurationPWM(); //pas accesible 
+    void configurationCTC();
+    //void ConfigurationNormal(); //pas sure de l'utiliser encore 
+
+};
 #endif
